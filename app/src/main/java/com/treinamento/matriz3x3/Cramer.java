db611386570e1576b0b9f9130db2521ab4a11c79
Kaique Gomes Machado <kaiquered@hotmail.com>
2021-05-06 16:54:01 -0300

    Versão 1.0 App Matriz 3x3

---
+++ app/src/main/java/com/treinamento/matriz3x3/Cramer.java
@@ -0,0 +1,86 @@
+package com.treinamento.matriz3x3;
+
+public class Cramer {
+
+    public static double valorX(double[][] m) {
+        return detX(m) / detM(m);
+    }
+
+    public static double valorY(double[][] m) {
+        return detY(m) / detM(m);
+    }
+
+    public static double valorZ(double[][] m) {
+        return detZ(m) / detM(m);
+    }
+
+    public static double detM(double[][] m) {
+
+        double[][] mS = new double[3][5];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+
+                if (j < 3)
+                    mS[i][j] = m[i][j];
+
+                if (j == 3 || j == 4)
+                    mS[i][j] = m[i][j - 3];
+            }
+        }
+        double del = (mS[0][0] * mS[1][1] * mS[2][2] + mS[0][1] * mS[1][2] * mS[2][0] + mS[0][2] * mS[1][0] * mS[2][1])
+                - (mS[0][2] * mS[1][1] * mS[2][0] + mS[0][0] * mS[1][2] * mS[2][1] + mS[0][1] * mS[1][0] * mS[2][2]);
+
+        return del;
+    }
+
+    private static double detX(double[][] m) {
+
+        double[][] mX = new double[3][3];
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+
+                if (j == 0)
+                    mX[i][j] = m[i][3];
+
+                else
+                    mX[i][j] = m[i][j];
+            }
+        return detM(mX);
+    }
+
+    private static double detY(double[][] m) {
+
+        double[][] mY = new double[3][3];
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+
+                if (j == 1)
+                    mY[i][j] = m[i][3];
+
+                else
+                    mY[i][j] = m[i][j];
+
+            }
+        return detM(mY);
+    }
+
+    private static double detZ(double[][] m) {
+
+        double[][] mZ = new double[3][3];
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+
+                if (j == 2)
+                    mZ[i][j] = m[i][3];
+
+                else
+                    mZ[i][j] = m[i][j];
+
+            }
+        return detM(mZ);
+    }
+}
---
+++ app/src/main/java/com/treinamento/matriz3x3/MainActivity.java
@@ -0,0 +1,111 @@
+package com.treinamento.matriz3x3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText[] elem = new EditText[12];
+    private double[][] matriz = new double[3][4];
+    private EditText E1x1;
+    private EditText E1x2;
+    private EditText E1x3;
+    private EditText E1x4;
+    private EditText E2x1;
+    private EditText E2x2;
+    private EditText E2x3;
+    private EditText E2x4;
+    private EditText E3x1;
+    private EditText E3x2;
+    private EditText E3x3;
+    private EditText E3x4;
+    private TextView X;
+    private TextView Y;
+    private TextView Z;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        elem[0] = findViewById(R.id.editText1);
+        elem[1] = findViewById(R.id.editText2);
+        elem[2] = findViewById(R.id.editText3);
+        elem[3] = findViewById(R.id.editTextX);
+        elem[4] = findViewById(R.id.editText4);
+        elem[5] = findViewById(R.id.editText5);
+        elem[6] = findViewById(R.id.editText6);
+        elem[7] = findViewById(R.id.editTextY);
+        elem[8] = findViewById(R.id.editText7);
+        elem[9] = findViewById(R.id.editText8);
+        elem[10] = findViewById(R.id.editText9);
+        elem[11] = findViewById(R.id.editTextZ);
+        X = findViewById(R.id.resultX);
+        Y = findViewById(R.id.resultY);
+        Z = findViewById(R.id.resultZ);
+
+        X.setText("0");
+        Y.setText("0");
+        Z.setText("0");
+    }
+
+    public boolean verifica() {
+
+        for (int i = 0; i < elem.length; i++) {
+            if (elem[i].getText().toString().equals("")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void calcular(View view) {
+
+        if (verifica() == true) {
+            Toast.makeText(getApplicationContext(), "Digite a Matriz completa.", Toast.LENGTH_LONG).show();
+        } else {
+            int k = 0;
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[0].length; j++) {
+
+                    if (i != 0)
+                        k = j + (int) Math.pow(2, (i + 1));
+                    else
+                        k = j;
+
+                    matriz[i][j] = Double.parseDouble(elem[k].getText().toString());
+                }
+            }
+
+            DecimalFormat df = new DecimalFormat("###.###");
+            df.setRoundingMode(RoundingMode.HALF_UP);
+            df.setMinimumFractionDigits(3);
+
+            X.setText(df.format(Cramer.valorX(matriz)));
+            Y.setText(df.format(Cramer.valorY(matriz)));
+            Z.setText(df.format(Cramer.valorZ(matriz)));
+        }
+    }
+
+    public void limpar(View view) {
+
+        for (int i = 0; i < elem.length; i++) {
+            elem[i].setText("");
+        }
+
+        X.setText("0");
+        Y.setText("0");
+        Z.setText("0");
+    }
+}
